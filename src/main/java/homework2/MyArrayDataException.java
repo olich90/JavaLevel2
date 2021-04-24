5cc815fb1f39f13ccfbf136967d428795072f761
olich90 <olich_90@mail.ru>
2021-04-24 17:11:36 +0300

    homework2

---
+++ src/main/java/homework2/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package homework2;
+
+public class MyArrayDataException extends Exception{
+
+    MyArrayDataException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/homework2/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package homework2;
+
+public class MyArraySizeException extends Exception{
+
+    MyArraySizeException(String message){
+        super(message);
+    }
+}
